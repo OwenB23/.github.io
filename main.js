dcc355380038934c6c151d8c4afb5f5407a70f19
OwenB23 <owenbond23@gmail.com>
2023-07-25 04:02:08 -0400

    Big changes to my Portfolio

--- main.js
+++ main.js
@@ -10,56 +10,95 @@ $(document).ready(function () {
     }
   });
 });
-// Fade in animation
 
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    console.log(entry);
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show");
+// Hello world typing animation
+window.addEventListener("DOMContentLoaded", () => {
+  const textContainer = document.querySelector(".hero-content");
+  const cursor = document.createElement("span");
+  cursor.classList.add("cursor");
+  textContainer.appendChild(cursor);
+
+  const text = "Hello World! My name is";
+  let index = 0;
+
+  function typeText() {
+    textContainer.textContent = text.slice(0, index);
+    index++;
+
+    if (index <= text.length) {
+      setTimeout(typeText, 150);
     } else {
-      entry.target.classList.remove("show");
+      cursor.style.display = "none";
+    }
+  }
+  typeText();
+});
+
+// projects
+$(document).ready(function () {
+  // Event listener to show the modal for the card click
+  $(".card-content-wrapper").on("click", function () {
+    const projectTitle = $(this).find(".card-header").text();
+    const projectDescription = $(this).find(".card-body p").text();
+
+    // Get the modal id from the clicked card
+    const modalId = $(this).data("target");
+    showModal(projectTitle, projectDescription, modalId);
+  });
+
+  // Function to show the modal
+  function showModal(projectTitle, projectDescription, modalId) {
+    const modalTitle = document.querySelector(`${modalId} .modal-title`);
+    const modalBody = document.querySelector(`${modalId} .modal-body`);
+
+    modalTitle.innerText = projectTitle;
+    modalBody.innerHTML = `<p>${projectDescription}</p>`;
+
+    // Show the Bootstrap modal
+    $(modalId).modal("show");
+  }
+  // Function to hide the modal
+  function hideModal() {
+    // Hide the Bootstrap modal
+    $("#myModal").modal("hide");
+  }
+
+  // Event listener to hide the modal when the close button is clicked
+  $(".close").on("click", hideModal);
+
+  // Event listener to hide the modal when clicked outside the modal area
+  $(window).on("click", function (event) {
+    if (event.target === document.getElementById("myModal")) {
+      hideModal();
     }
   });
 });
 
-const hiddenElements = document.querySelectorAll(
-  ".github-container, .navbar, .about-me-container, .get-in-touch-container, .project-container, .project-header, .linked-in"
-);
-hiddenElements.forEach((el) => observer.observe(el));
+// Contact Form
+(function () {
+  emailjs.init("owenbond23@gmail.com");
 
-// projects
+  document
+    .getElementById("contactForm")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
 
-function openCity(evt, projectName) {
-  var i, tabcontent, tablinks;
+      const form = event.target;
+      const data = {
+        name: form.name.value,
+        email: form.email.value,
+        message: form.message.value,
+      };
 
-  var projectDescription = document.getElementById(projectName);
-
-  var isVisible = projectDescription.style.display === "flex";
-
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].classList.remove("active");
-  }
-
-  if (!isVisible) {
-    projectDescription.style.display = "flex";
-    projectDescription.style.justifyContent = "flex-start";
-    projectDescription.style.alignItems = "center";
-    projectDescription.style.flexDirection = "column";
-    evt.currentTarget.classList.add("active");
-  }
-}
-
-// Contact
-
-const button = document.getElementById("openFormButton");
-
-button.addEventListener("click", function () {
-  window.location.href = "mailto:owenbond23@gmail.com";
-});
+      emailjs.send("service_2xveffh", "template_fuznnpt", data).then(
+        function (response) {
+          alert("Message sent successfully!");
+          form.reset();
+        },
+        function (error) {
+          alert("Oops! Something went wrong. Please try again.");
+          console.log("Error:", error);
+        }
+      );
+    });
+})();
